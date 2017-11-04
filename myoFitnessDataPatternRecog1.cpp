ca16c8d1ee5c20ea1696a66cfd00eafd6445e7c7
aneeshsri <aneeshsas@gmail.com>
2017-11-04 05:59:17 -0400

    Myo Fitness Data Pattern Recognition Algorithm

---
+++ myoFitnessDataPatternRecog1.cpp
@@ -0,0 +1,11 @@
+//Currently pseudo-code to nail down algorithm:
+float rollSpot.Value = current excelRollSpot.Value
+
+if rollSpot.Value >= 1.9
+	
+	If (rollSpot.Value) <= (rollSpot.Value + 1)
+	
+	Else
+		  
+Else
+	//Nothing
